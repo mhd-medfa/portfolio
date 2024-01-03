6cab4962d1382b818f6650a52d6c373fa13ee2bd
Mohamad Al Mdfaa <mohamad.almdfaa@gmail.com>
2024-01-03 17:59:14 +0000

    refactor: update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
           class="col-xl-6 col-bg-6 col-md-6 col-sm-12 pbelow"
           style="color: white;"
         >
-          <span>© 2020 Copyright: Hrishikesh Paul</span>
+          <span></span>
         </div>
 
         <div class="col-xl-6 col-bg-6 col-md-6 col-sm-12">
